035984183cd009c6d0005d2e54ccca33a8384d6b
Monty Carter <montgomery.carter@gmail.com>
2015-03-26 19:51:12 -0700

    VERY basic setup script and run script

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd paramILSConfig
+ruby ../paramils2.3.7-source/param_ils_2_3_run.rb -numRun 0 -scenariofile scenario-smack.txt
+cd ..
---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install ruby-full
+
+wget http://www.cs.ubc.ca/labs/beta/Projects/ParamILS/paramils2.3.7-source.tgz
+tar -zvxf paramils2.3.7-source.tgz
+
