0986757e5308d8a4c67b7aa0501fde5574d7b93a
Monty Carter <montgomery.carter@gmail.com>
2015-04-26 13:29:11 -0600

    Added algo_specifics.rb patch to add geomean10, plus updated setup.sh script to copy this patch over existing algo_specifics.rb after paramils install

--- setup.sh
+++ setup.sh
@@ -6,3 +6,6 @@ sudo apt-get install ruby-full
 wget http://www.cs.ubc.ca/labs/beta/Projects/ParamILS/paramils2.3.7-source.tgz
 tar -zvxf paramils2.3.7-source.tgz
 
+# Patch default algo_specifics with geomean10, which penalizes wrong results
+#  for overall objective
+cp algoPatchParamils/algo_specifics.rb ./paramils2.3.7-source/
