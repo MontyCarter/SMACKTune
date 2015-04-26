59c839a28688cdb82270de3e762dec5d9a7f5f7c
Monty Carter <montgomery.carter@gmail.com>
2015-04-26 14:00:53 -0600

    Updated generateInstanceFile.py to give summary of SAT,UNSAT,TIMEOUT counts for all benchmarks in instance file, as well as total time for all benchmarks to run

--- smackConfig/generateInstanceFile.py
+++ smackConfig/generateInstanceFile.py
@@ -6,34 +6,48 @@ import os
 from wrapperSmack import *
 
 dfltTimeout = 30
-dfltArgs = ['--unroll', '8']
+dfltArgs = ['--unroll', '12']
 
 
 def callSmackWrapper(instanceName):
     addArgs = dfltArgs
     output, result, runtime, runlength, best_sol, seed = get_result(*run(instanceName, dfltTimeout, addArgs))
-    return [instanceName, str(runtime), result]
+    return [instanceName, runtime, result]
 
 def genInstanceFile(folder):
     #Ensure trailing slash
     folder = folder if folder[-1] == "/" else folder + "/"
     res = []
-    longestFile = 0
-    longestFloat = 0
+    longestFile = longestFloat = satCnt = unsatCnt = timeoutCnt = totalTime = 0
+    #Collect results from each benchmark
     for inFile in sorted(glob.glob(folder + "*.c")):
         res.append(callSmackWrapper(inFile))
         #track longest filename for printing alignment
         longestFile = longestFile if len(res[-1][0])<=longestFile else len(res[-1][0])
-        longestFloat = longestFloat if len(res[-1][1])<=longestFloat else len(res[-1][1])
+        longestFloat = longestFloat if len(str(res[-1][1]))<=longestFloat else len(str(res[-1][1]))
+        if res[-1][2] == "SAT":
+            satCnt += 1
+        elif res[-1][2] == "UNSAT":
+            unsatCnt += 1
+        elif res[-1][2] == "TIMEOUT":
+            timeoutCnt += 1
+        totalTime += res[-1][1]
         #print so we can see progress
-        print(" ".join(res[-1]))
+        print(" ".join(map(str,res[-1])))
 
+    #Convert from list of lists to list of output lines
     toFile = []
     for inst in res:
         #Align printing
         toFile.append(inst[0] + " "*(longestFile-len(inst[0])+1) + 
-                      inst[1] + " "*(longestFloat-len(inst[1])+1) + 
+                      str(inst[1]) + " "*(longestFloat-len(str(inst[1]))+1) + 
                       inst[2])
+
+    print("Summary:")
+    print("\tSAT Count:\t" + str(satCnt))
+    print("\tUNSAT Count:\t" + str(unsatCnt))
+    print("\tTIMEOUT Count:\t" + str(timeoutCnt))
+    print("\tTotal Runtime:\t" + str(totalTime))
 
     return toFile
 
